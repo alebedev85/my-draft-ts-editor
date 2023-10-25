4cc689b59cc8060fa151bb2cae8e5aaaa3d300cd
Andrey Lebedev <alebedev424@gmail.com>
2023-10-25 11:14:21 +0300

    refactor: сделал свои стили для приложения

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import TextEditor, { TextEditorProvider } from "./TextEditor";
 import ToolPanel from "./ToolPanel/ToolPanel";
+import "./App.scss";
 
 
 function App() {
   return (
     <TextEditorProvider >
-      <ToolPanel />
-      <TextEditor />
+      <header className="app-header">
+        My Draft Text Editor
+      </header>
+      <main className="page">
+        <div className="editr-wrap">
+          <ToolPanel />
+          <TextEditor />
+        </div>
+      </main>
     </TextEditorProvider>
   );
 }
