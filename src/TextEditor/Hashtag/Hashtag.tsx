3da911fba173448d501ca4000a5c267942385aaf
Andrey Lebedev <alebedev424@gmail.com>
2023-10-31 13:54:26 +0300

    feater: сделал вывод текста хештега в консоль

--- src/TextEditor/Hashtag/Hashtag.tsx
+++ src/TextEditor/Hashtag/Hashtag.tsx
@@ -7,7 +7,8 @@ import { useEditorApi } from '../context';
 type HashtagProps = {
   children: React.ReactNode;
   contentState: ContentState;
-  entityKey: string;
+  blockKey: string;
+  decoratedText: string
 }
 
 /**
@@ -15,25 +16,11 @@ type HashtagProps = {
  * @param объект типа HashtagkProps
  * @returns разметка с сылкой
  */
-const Hashtag = ({ contentState, entityKey, children }: HashtagProps) => {
-  // const text = contentState.getData();
+const Hashtag = ({ contentState, blockKey, children, decoratedText }: HashtagProps) => {
 
   const handlerClick = () => {
-    console.log(entityKey)
+    console.log(decoratedText)
   }
-
-  // /* Получаем url с помощью уникального ключа Entity */
-  // const { url } = contentState.getEntity(entityKey).getData();
-
-  // /**
-  //  * Обрабатываем клик в компоненте
-  //  */
-  // const handlerClick = () => {
-  //   const newUrl = prompt('URL:', url);
-  //   if (newUrl) {
-  //     setEntityData(entityKey, { url: newUrl });
-  //   }
-  // }
 
   return (
     <span className='hashtag' onClick={handlerClick}>
